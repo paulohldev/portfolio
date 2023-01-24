e0c996a08651d765aa029d293117e4f7c640190b
paulohldev <paulo85542080@gmail.com>
2023-01-24 18:11:50 -0300

    funcionamento dos botoes do slide

---
+++ scripts/modules/slide.js
@@ -0,0 +1,55 @@
+export default function initSlide() {
+  const slideSection = document.querySelector('.slide-section');
+  const slide = slideSection.querySelector('.slide');
+  const slidesElements = slide.querySelectorAll('li');
+  const nextButton = document.querySelector('.controllers .next');
+  const previousButton = document.querySelector('.controllers .previous');
+  const slideElementWidth = slidesElements[0].getBoundingClientRect().width;
+  const slideSectionWidth = slideSection.getBoundingClientRect();
+  const stringCurrentSlide = 'current-slide';
+  console.log(slideSectionWidth);
+
+  const moveSlide = (currentSlide, nextOrPrevSlide) => {
+    if (nextOrPrevSlide) {
+      slide.style.transform = `translateX(-${nextOrPrevSlide.offsetLeft}px)`;
+      toggleCurrentClass(currentSlide, nextOrPrevSlide);
+    } else {
+      toggleCurrentClass(currentSlide, nextOrPrevSlide);
+      slide.style.transform = `translateX(${nextOrPrevSlide}px)`;
+    }
+  };
+
+  const toggleCurrentClass = (currentSlide, nextOrPrevSlide) => {
+    currentSlide.classList.remove(stringCurrentSlide);
+    nextOrPrevSlide.classList.add(stringCurrentSlide);
+  };
+
+  const nextSlide = (currentSlide) => {
+    const nextSlide = currentSlide.nextElementSibling;
+    const firstSlide = nextSlide === null ? slidesElements[0] : nextSlide;
+    moveSlide(currentSlide, firstSlide);
+  };
+
+  const previousSlide = (currentSlide) => {
+    const previousSlide = currentSlide.previousElementSibling;
+    const lastSlide =
+      previousSlide === null
+        ? slidesElements[slidesElements.length - 1]
+        : previousSlide;
+    moveSlide(currentSlide, lastSlide);
+  };
+
+  nextButton.addEventListener('click', () => {
+    const currentSlide = Array.from(slidesElements).find((slide) => {
+      return slide.classList.contains(stringCurrentSlide);
+    });
+    nextSlide(currentSlide);
+  });
+
+  previousButton.addEventListener('click', () => {
+    const currentSlide = Array.from(slidesElements).find((slide) => {
+      return slide.classList.contains(stringCurrentSlide);
+    });
+    previousSlide(currentSlide);
+  });
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 import animaScroll from './modules/animaScroll.js';
 import scrollLink from './modules/scrollLink.js';
 import menuMobile from './modules/menuMobile.js';
+import initSlide from './modules/slide.js';
 
 menuMobile();
 animaScroll();
 scrollLink();
+initSlide();
